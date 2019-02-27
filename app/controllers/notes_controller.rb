b6696d80ca03d99abbc2f1f1c7759aa1bc7bc677
Cookie McCormick <muntananuchat@gmail.com>
2019-02-27 16:31:44 -0500

    moved images folder, created new and edit notes in views/controller

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,49 @@
 class NotesController < ApplicationController
+  get '/notes/new' do
+    if logged_in?
+      erb :'/notes/new'
+    else
+      redirect '/login'
+    end
+  end
+
+  post '/notes' do
+    if logged_in?
+      if params[:note][:content].present?
+        @note = current_user.notes.create(content: params[:note][:content])
+        redirect '/dashboard'
+      else
+        redirect '/notes/new'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/notes/:id/edit' do
+    if logged_in?
+      @note = current_user.notes.find_by(id: params[:id])
+      if @note
+        erb :'/notes/edit'
+      else
+        redirect '/dashboard'
+      end
+    else
+      redirect '/login'
+    end
+  end
+
+  patch '/notes/:id' do
+    if logged_in?
+      @note = current_user.notes.find_by(id: params[:id])
+      if params[:note][:content].present?
+        @note.update(content: params[:note][:content])
+        redirect '/dashboard'
+      else
+        redirect "/notes/#{@note.id}/edit"
+      end
+    else
+      redirect '/login'
+    end
+  end
 end
