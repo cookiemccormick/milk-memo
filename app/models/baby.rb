293bc5ab1d060b4854939b9016ca01a287f7c9ec
Cookie McCormick <muntananuchat@gmail.com>
2019-03-01 13:00:04 -0500

    added constant SIZES and updated baby dashboard

--- app/models/baby.rb
+++ app/models/baby.rb
@@ -6,7 +6,43 @@ class Baby < ActiveRecord::Base
   GENDERS = [MALE, FEMALE, UNKNOWN]
 
   SIZES = {
-    "21" => "Carrot"
+    "4" => "poppy seed",
+    "5" => "apple seed",
+    "6" => "sweet pea",
+    "7" => "blueberry",
+    "8" => "kidney bean",
+    "9" => "grape",
+    "10" => "tangerine",
+    "11" => "fig",
+    "12" => "lime",
+    "13" => "pea pod",
+    "14" => "lemon",
+    "15" => "apple",
+    "16" => "avocado",
+    "17" => "pear",
+    "18" => "sweet potato",
+    "19" => "mango",
+    "20" => "banana",
+    "21" => "carrot",
+    "22" => "squash",
+    "23" => "grapefruit",
+    "24" => "corn",
+    "25" => "acorn squash",
+    "26" => "head of kale",
+    "27" => "cauliflower",
+    "28" => "eggplant",
+    "29" => "butternut squash",
+    "30" => "cabbage",
+    "31" => "coconut",
+    "32" => "chinese cabbage",
+    "33" => "pineapple",
+    "34" => "melon",
+    "35" => "honeydew",
+    "36" => "canary melon",
+    "37" => "swiss chard",
+    "38" => "rhubarb",
+    "39" => "small watermelon",
+    "40" => "watermelon"
   }
 
   belongs_to :user
