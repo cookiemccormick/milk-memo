a1d261f1fd70aa6930abc245c82aec2223e865d3
Cookie McCormick <muntananuchat@gmail.com>
2019-03-02 22:29:13 -0500

    reworked baby edit form

--- public/js/app.js
+++ public/js/app.js
@@ -7,3 +7,8 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.timepicker');
   var instances = M.Timepicker.init(elems, {});
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems, {});
+});
